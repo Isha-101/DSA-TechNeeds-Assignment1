5daf5620fd97662aa69e9c01d702d2f123920fde
Isha-101 <145088595+Isha-101@users.noreply.github.com>
2023-10-29 18:59:25 +0530

    Answer of Q4

--- A1_Q4.cpp
+++ A1_Q4.cpp
@@ -2,14 +2,11 @@
 using namespace std;
 int main()
 {
-    int low, high;
+    int low,high;
     cout<<"Enter lower bound of range ";
     cin>>low;
     cout<<"Enter upper bound of range ";
     cin>>high;
-    if(low>high) { int l=high; int h=low;}
-    int l=low;
-    int h=high;
     cout<<"Prime numbers btw given range are\n ";
     for(int i=l; i<=h; i++)
     {
