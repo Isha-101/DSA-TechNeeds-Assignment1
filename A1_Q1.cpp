86e5c2cfbeebbb9bfe96ac174106502d06bf14af
Isha-101 <145088595+Isha-101@users.noreply.github.com>
2023-10-29 17:26:29 +0530

    Answer of Q1

--- A1_Q1.cpp
+++ A1_Q1.cpp
@@ -1,11 +1,15 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"*****"<<endl;
-    cout<<"   *"<<endl;
-    cout<<"  *"<<endl;
-    cout<<" *"<<endl;
-    cout<<"*****"<<endl;
-    return 0;
+#include <iostream>
+
+int main() {
+  for (int i=0; i<5; i++) {
+    for (int j=0; j<5; j++) {
+      if (i==0 || j+i==4 || i==4) {
+        std::cout << "*";
+      } else {
+        std::cout << " ";
+      }
+    }
+    std::cout << std::endl; //for inserting new lines in btw star patterns
+  }
+  return 0;
 }
