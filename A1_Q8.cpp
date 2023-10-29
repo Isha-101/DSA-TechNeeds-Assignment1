08408230f0c1aec116e56bcb7bda459b6cb8ac2f
Isha-101 <145088595+Isha-101@users.noreply.github.com>
2023-10-29 18:26:19 +0530

    Answer of Q8

---
+++ A1_Q8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,k;
+    int j=1;
+    cout<<"Enter number ";
+    cin>>n;
+    cout<<"Enter number of digits to be rotated ";
+    cin>>k;
+    for(int i=1; i<=k; i++){
+        j=10*j;
+    }
+    cout<<n%j<<n/j;
+    return 0;
+}
