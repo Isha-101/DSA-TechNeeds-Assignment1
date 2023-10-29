8f3f0982bc4ffb5a6b79ba2c81d8b36e84fa7f54
Isha-101 <145088595+Isha-101@users.noreply.github.com>
2023-10-29 17:06:30 +0530

    Answer of Q2

---
+++ A2_Q2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m;
+    cout<<"Enter your Marks ";
+    cin>>m;
+    if(m>90)
+      cout<<"Excellent \n";
+    else if(m>80)
+      cout<<"Good \n";
+    else if(m>70)
+      cout<<"Fair \n";
+    else if(m>60)
+      cout<<"Meets expectations \n";
+    else
+      cout<<"Below par \n";
+    return 0;
+}
